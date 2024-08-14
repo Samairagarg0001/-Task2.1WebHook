b696dd346c884aeb4f387429bb1dbebc994d82fb
Samairagarg0001 <gargsamaira4@gmail.com>
2024-08-14 15:39:57 +0530

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Samaira"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "87654321"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2626352			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "SDW8PIS716FXWVCI"   // replace XYZ with your channel write API Key
